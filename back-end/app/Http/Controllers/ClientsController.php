40c49eb6a822f87672c5ed932436dcbbfa0b155c
Simeon Raykov <makenr3@gmail.com>
2020-05-10 13:38:51 +0300

    Add files via upload
    update membership API

--- back-end/app/Http/Controllers/ClientsController.php
+++ back-end/app/Http/Controllers/ClientsController.php
@@ -5,22 +5,43 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Client;
 use Symfony\Component\Console\Output\ConsoleOutput;
-
+use Illuminate\Support\Facades\DB;
 
 class ClientsController extends Controller
 {
 
-    public function index(Request $request)
+    public function index()
     {
-        $output = new ConsoleOutput();
-        $output->writeln($request);
-        echo "data";
+        $clients = DB::table('clients')->get();
+        return $clients;
     }
 
     public function store(Request $request)
     {
-        $output = new ConsoleOutput();
-        $output->writeln($request);
-        echo "data";
+        $client = new Client;
+        $client->name = $request->input('clientName');
+        $client->money_transfered = $request->input('price');
+        $client->membership_valid = $request->input('expirationDate');
+        $client->save();
+        return http_response_code(201);
     }
-} 
+
+    public function updateMembership(Request $request)
+    {
+        $clientName = $request->input('clientName');
+        $price = $request->input('price');
+        $expirationDate = $request->input('expirationDate');
+        Client::where('name', $clientName)->update(
+            [
+                'membership_valid' => $expirationDate,
+                'money_transfered' => DB::raw('money_transfered + ' . $price)
+            ],
+        );
+    }
+
+    public function remove(Request $request)
+    {
+        $clientName = $request->input('clientName');
+        Client::where('name', $clientName)->delete();
+    }
+}
